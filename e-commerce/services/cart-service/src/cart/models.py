a59eb5594c6b125997c0cd1cd85fa08fea52d5c6
Lameck Irungu <mugolameck@gmail.com>
2026-02-07 12:07:51 +0300

    feat(cart): add initial migration, models, and serializers for Cart and CartItem

--- e-commerce/services/cart-service/src/cart/models.py
+++ e-commerce/services/cart-service/src/cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    user_id = models.UUIDField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product_id = models.UUIDField()
+    product_name = models.CharField(max_length=200)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ("cart", "product_id")
